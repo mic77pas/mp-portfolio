75c3b02fc01c3daf153458e3638bd055cb6d1d48
Michael Pasyechnyk <mic77p@gmail.com>
2025-08-22 00:24:39 -0400

    Footer updated

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,6 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    <footer
+      className="flex justify-start items-end w-full h-12 px-6 py-3 font-medium text-[#98b493] bg-[#202620]"
+    >
       &copy; {new Date().getFullYear()} | Michael Pasyechnyk
     </footer>
   );
