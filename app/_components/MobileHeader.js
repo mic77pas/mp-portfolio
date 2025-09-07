f1645e30c6c1f7b3e4c1785e4c10ca86962d5e0b
Michael Pasyechnyk <mic77p@gmail.com>
2025-09-07 18:50:08 -0400

    Mobile header dropdown updated

--- app/_components/MobileHeader.js
+++ app/_components/MobileHeader.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiMail, HiMailOpen, HiOutlineDotsVertical } from "react-icons/hi";
+import { IoDocumentText } from "react-icons/io5";
 import Logo from "./Logo";
 
 export default function MobileHeader() {
@@ -46,7 +47,7 @@ export default function MobileHeader() {
         <div
           className="absolute bottom-full mb-4 right-0
   bg-[#0c0d0c]/60 backdrop-blur-md rounded-2xl shadow-lg
-  flex flex-col items-center gap-4 p-4 text-[#c4dbbf] text-shadow-[#0f1a0c] text-shadow-2xs font-semibold"
+  flex flex-col items-center gap-4 p-4 text-[#c4dbbf] text-shadow-[#0f1a0c] text-shadow-2xs font-semibold -translate-x-3.5"
         >
           <Link
             href="/resume.pdf"
@@ -54,32 +55,30 @@ export default function MobileHeader() {
             rel="noopener noreferrer"
             className="hover:text-[#98B493] transition-colors duration-300"
           >
-            resume
+            <IoDocumentText className="w-7 h-7" />
           </Link>
-          <div className="flex gap-4 mt-1">
-            <a
-              href="mailto:mic77p@gmail.com"
-              className="group flex items-center gap-2 hover:text-[#98B493] transition-colors duration-300"
-            >
-              <HiMail className="w-7 h-7 -translate-y-0.5" />
-            </a>
-            <Link
-              href="https://linkedin.com/in/michaelpasyechnyk"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-[#98B493] transition-colors duration-300"
-            >
-              <FaLinkedin className="w-6 h-6" />
-            </Link>
-            <Link
-              href="https://github.com/mic77pas"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-[#98B493] transition-colors duration-300"
-            >
-              <FaGithub className="w-6 h-6" />
-            </Link>
-          </div>
+          <a
+            href="mailto:mic77p@gmail.com"
+            className="group flex items-center gap-2 hover:text-[#98B493] transition-colors duration-300"
+          >
+            <HiMail className="w-7 h-7" />
+          </a>
+          <Link
+            href="https://linkedin.com/in/michaelpasyechnyk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-[#98B493] transition-colors duration-300"
+          >
+            <FaLinkedin className="w-6 h-6" />
+          </Link>
+          <Link
+            href="https://github.com/mic77pas"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-[#98B493] transition-colors duration-300"
+          >
+            <FaGithub className="w-6 h-6" />
+          </Link>
         </div>
       )}
     </div>
