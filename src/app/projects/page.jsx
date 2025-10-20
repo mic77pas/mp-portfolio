fa24085b3a565698d6b8b6d9ea09b3f6bb2bf6a9
Michael Pasyechnyk <mic77p@gmail.com>
2025-10-20 19:20:02 -0400

    Projects spacing updated

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -43,7 +43,7 @@ export default function Page() {
         // enableBlur={true}
         // baseRotation={2}
         // blurStrength={8}
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto"
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto mb-10"
       >
         {filteredProjects.map((project) => (
           <div
